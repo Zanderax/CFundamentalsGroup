486bd015dec5742a611dde255f01c3551c966da0
David <davidjamesbelcher0@gmail.com>
2017-05-21 14:28:53 -0700

    Did more splitting into files for existing functionality.

--- huffman.h
+++ huffman.h
@@ -1,26 +1,15 @@
+#ifndef HUFFMAN
+#define HUFFMAN
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include "dataTypes.h"
+#include "bytelist.h"
+#include "huffmanTree.h"
 #include "huffmanCode.h"
 
-char* encode( char*, uint );
-bool isIn( byte*, uint, byte );
-uint countInList( byte*, uint, byte );	
-ByteList* createByteList( byte*, uint );
-bool isInElements( ByteListElement*, uint, byte );
-uint countUnique( byte*, uint );
-void sortByteList ( ByteList*, uint );
-void swapBytes ( ByteList*, uint, uint );
-void populateByteList ( ByteList*, byte*, uint );
-ByteList* initializeByteList( byte*, uint );
-Node* createHuffmanTree( ByteList* );
-void printHuffmanTree( Node* );
-uint findSmallestRoot( Node**, uint );
-uint findSecondSmallestRoot( Node**, uint );
-void addRoot( Node**, uint, Node*, Node* );
-void removeRoot( Node**, uint, uint );
-void combineRoots( Node**, uint, uint, uint );
-Node* makeLeaf( ByteListElement* );
-uint countRoots( Node**, uint );
+byte* encode( byte*, uint );
+
+#endif /* HUFFMAN */
